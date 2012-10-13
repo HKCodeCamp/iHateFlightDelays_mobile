89d8682ce6a89a6733534a7c3f231753a05eecf1
Xiaobing S <xiaobing_shi@yahoo.com>
2012-10-13 10:04:43 +0800

    dev

---
+++ php/login.php
@@ -0,0 +1,13 @@
+<?php
+    $username = $_POST[username];
+    $password = $_POST[password];    
+    
+$item = array();
+$item['username'] = $username;
+$item['status'] = 'success';
+
+//echo '{"item":'. json_encode($item) .'}'; 
+
+echo json_encode($item); 
+	
+?>
